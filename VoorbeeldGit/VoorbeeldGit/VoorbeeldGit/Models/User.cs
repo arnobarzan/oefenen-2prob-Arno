086bf8ccd492fc1968f71f6cf7eae160effde498
arnobarzan <arno.barzan@pxl.be>
2020-11-16 14:02:33 +0100

    Create User model

---
+++ VoorbeeldGit/VoorbeeldGit/VoorbeeldGit/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VoorbeeldGit.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+    }
+}
